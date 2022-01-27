c2ed7986b45465f5f8c8a0e6a623811d3f2a2d36
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-01-26 20:36:14 -0500

    webpack reconfig in vue.config.js due to error upon npm run serve

--- vue-weather/vue.config.js
+++ vue-weather/vue.config.js
@@ -1,3 +1,15 @@
 module.exports = {
     publicPath: '/vue-weather/'
-  };
+};
+
+//vue.config.js
+module.exports = {
+  chainWebpack: config => {
+      config
+          .plugin('html')
+          .tap(args => {
+              args[0].title = "My Vue App";
+              return args;
+          })
+  }
+}
